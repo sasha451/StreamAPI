f4e5c95601caed3a092115eb62c319c4ddd2c704
sasha451 <shulga.sasha.new@gmail.com>
2019-10-17 21:05:10 +0300

    feat: implement isCharacterPresentInAllNames()

--- stream-api/src/main/java/by/courses/java/streamapi/operation/DefaultStream.java
+++ stream-api/src/main/java/by/courses/java/streamapi/operation/DefaultStream.java
@@ -36,7 +36,7 @@ public class DefaultStream implements Operation<UserBase> {
 
     @Override
     public boolean isCharacterPresentInAllNames(Collection<UserBase> entities, String character) {
-        return false;
+        return entities.stream().allMatch(user -> user.getName().toLowerCase().contains(character.toLowerCase()));
     }
 
     @Override
