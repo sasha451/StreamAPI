74737bdf59d7dfbd28676c9076a6f6314c800045
sasha451 <shulga.sasha.new@gmail.com>
2019-10-17 13:39:16 +0300

    feat: getters and setters add

--- stream-api/src/main/java/by/courses/java/streamapi/entity/UserBase.java
+++ stream-api/src/main/java/by/courses/java/streamapi/entity/UserBase.java
@@ -8,6 +8,22 @@ import lombok.Data;
 public class UserBase {
 
     private String name;
-
     private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
