e09218c625c8e23f517782b70d1b32abce080215
sasha451 <shulga.sasha.new@gmail.com>
2019-10-17 21:02:31 +0300

    feat: implement getTwoUsersStartingFromSecond()

--- stream-api/src/main/java/by/courses/java/streamapi/operation/DefaultStream.java
+++ stream-api/src/main/java/by/courses/java/streamapi/operation/DefaultStream.java
@@ -31,7 +31,7 @@ public class DefaultStream implements Operation<UserBase> {
 
     @Override
     public Collection<UserBase> getTwoUsersStartingFromSecond(Collection<UserBase> entities) {
-        return null;
+        return entities.stream().skip(1).limit(2).collect(Collectors.toList());
     }
 
     @Override
